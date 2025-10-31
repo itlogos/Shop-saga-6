26e79c265946c7c14daeeffdbd443a72d63b9a30
olegnik <olegnik11@ukr.net>
2025-10-31 11:02:37 +0200

    Add package-lock for web-app

---
+++ order-service/src/main/java/com/example/order/model/OrderItem.java
@@ -0,0 +1,14 @@
+package com.example.order.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable @Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class OrderItem {
+  private Long productId;
+  private Integer quantity;
+  private Double price;
+}
